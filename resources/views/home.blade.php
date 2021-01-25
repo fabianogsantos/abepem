16187c194853974fc120622a1fe5e1235a1043e3
Fabiano Santos <fabianogs@gmail.com>
2021-01-24 23:01:00 -0300

    Mais um teste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ABEPEM</title>
+    <title>ABEPEM 2021</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 </head>
 <body>
